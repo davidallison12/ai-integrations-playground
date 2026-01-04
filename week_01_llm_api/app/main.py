6f9c676499c22194449c1172bb549798741031df
david-allison12 <dgreenal@gmail.com>
2026-01-04 15:35:58 -0500

    Added API key + example using Open AI

--- week_01_llm_api/app/main.py
+++ week_01_llm_api/app/main.py
@@ -1,8 +1,25 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from openai import OpenAI
 from pydantic import BaseModel # Used for request body
 from typing import Literal
+import os
 
+# ==========================
+# LOAD ENVIRONEMENT VARIABLE
+# ==========================
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise RuntimeError("OPENAI_API_KEY not set")
+
+# ==========================
+# API ENDPOINTS
+# ==========================
 app = FastAPI()
+client = OpenAI()
 
 # Health Endpoint 
 @app.get("/health")
@@ -10,12 +27,14 @@ async def health():
     return {"status": "ok"}
 
 
+# ===================================
 # POST /analyze
 # input: text
 # output: summary + sentiment (w/ request Id, confidence + fallback used boolean)
 # constraints:
 # - structured JSON
 # - deterministic
+# ===================================
 
 # Note: Can use Literal[] in order to create specific approved values or use Enum
 class Summary(BaseModel):
@@ -26,3 +45,20 @@ class Summary(BaseModel):
 @app.post("/analyze/")
 async def create_summary(summary: Summary): # Define request body template here
     return summary
+
+
+
+# You must pay to use any of the models / Not ideal for those just looking to learn in a cheap manner
+# Pay attention to usage in order to manage spend 
+
+
+# ==========================
+# TEST EXAMPLE USING OPEN AI
+# ==========================
+# response = client.responses.create(
+#     model="gpt-5-nano", # This looks to be the cheapest model you can use. 
+#     input="Write a one-sentence bedtime story about a unicorn."
+# )
+
+# print(response.output_text) 
+# Response: Under the silver moon, a gentle unicorn curled up on a bed of clover and drifted into a dream where the stars sang lullabies.
